e33d0f5b7d3fa8369fac551030399cbd16f5b31f
Liza Detrick <114438185+L2D2Grafana@users.noreply.github.com>
2024-04-02 14:24:39 -0700

    Update src/utils/shared.ts
    Co-authored-by: Cyril Tovena <cyril.tovena@gmail.com>

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -22,7 +22,7 @@ export const VAR_DATASOURCE = 'ds';
 export const VAR_DATASOURCE_EXPR = '${ds}';
 export const VAR_LOGS_FORMAT = 'logsFormat';
 
-export const LOG_STREAM_SELECTOR_EXPR = `${VAR_FILTERS_EXPR} ${VAR_PATTERNS_EXPR}${VAR_FIELDS_EXPR}`;
+export const LOG_STREAM_SELECTOR_EXPR = `${VAR_FILTERS_EXPR} ${VAR_PATTERNS_EXPR} ${VAR_FIELDS_EXPR}`;
 export const explorationDS = { uid: VAR_DATASOURCE_EXPR };
 
 export const ALL_VARIABLE_VALUE = '$__all';
