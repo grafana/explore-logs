364c30212dffb2149a98674bcb2aa21622ef26e6
Matias Chomicki <matyax@gmail.com>
2024-04-02 19:35:58 +0200

    feat(lineFilter): debounce and set variable value

--- src/components/Explore/LogsByService/LineFilter.tsx
+++ src/components/Explore/LogsByService/LineFilter.tsx
@@ -1,7 +1,9 @@
 import { css } from "@emotion/css";
-import { SceneComponentProps, SceneObjectBase, SceneObjectState } from "@grafana/scenes";
+import { CustomVariable, SceneComponentProps, SceneObjectBase, SceneObjectState, sceneGraph } from "@grafana/scenes";
 import { Field, Input } from "@grafana/ui";
+import { debounce } from "lodash";
 import React, { ChangeEvent } from "react";
+import { VAR_LINE_FILTER } from "utils/shared";
 
 interface LineFilterState extends SceneObjectState {
 	search: string;
@@ -14,17 +16,29 @@ export class LineFilter extends SceneObjectBase<LineFilterState> {
 		super({ search: '', ...state });
 	}
 
+	private getVariable() {
+    const variable = sceneGraph.lookupVariable(VAR_LINE_FILTER, this);
+    if (!(variable instanceof CustomVariable)) {
+      throw new Error('Logs format variable not found');
+    }
+    return variable;
+  }
+
 	handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		this.setState({
 			search: e.target.value,
 		});
+		this.updateVariable(e.target.value);
 	}
+
+	updateVariable = debounce((search: string) => {
+		const variable = this.getVariable();
+		variable.changeValueTo(`|= \`${search}\``);
+	}, 350);
 }
 
 function LineFilterRenderer({ model }: SceneComponentProps<LineFilter>) {
   const { search } = model.useState();
-
-	console.log(search);
 
   return (
     <Field>
