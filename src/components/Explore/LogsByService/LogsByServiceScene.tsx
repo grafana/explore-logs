62d49cb3266642da5c5820fd62fae7b81da2b560
Matias Chomicki <matyax@gmail.com>
2024-04-02 19:35:33 +0200

    feat(logsByServiceScene): add variable for the line search

--- src/components/Explore/LogsByService/LogsByServiceScene.tsx
+++ src/components/Explore/LogsByService/LogsByServiceScene.tsx
@@ -34,6 +34,7 @@ import {
   LOG_STREAM_SELECTOR_EXPR,
   VAR_DATASOURCE_EXPR,
   EXPLORATIONS_ROUTE,
+  VAR_LINE_FILTER,
 } from '../../../utils/shared';
 import { getDatasource, getExplorationFor } from '../../../utils/utils';
 import { ShareExplorationButton } from './ShareExplorationButton';
@@ -85,7 +86,7 @@ export class LogsByServiceScene extends SceneObjectBase<LogSceneState> {
       body: state.body ?? buildGraphScene(),
       $variables:
         state.$variables ??
-        new SceneVariableSet({ variables: [new CustomVariable({ name: VAR_LOGS_FORMAT, value: '' })] }),
+        new SceneVariableSet({ variables: [new CustomVariable({ name: VAR_LOGS_FORMAT, value: '' }), new CustomVariable({ name: VAR_LINE_FILTER, value: '' })] }),
       $data: new SceneQueryRunner({
         datasource: explorationDS,
         queries: [buildQuery()],
