28618ca06e7241477e1d182aa9c6189abb0488df
Sven Grossmann <svennergr@gmail.com>
2024-04-03 09:42:21 +0200

    scope access to `Editors` or users with `grafana-lokiexplore-app:read`

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -3,12 +3,19 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import { prefixRoute } from '../../utils/utils.routing';
 import { ROUTES } from '../../constants';
 import { LogExplorationPage } from '../../pages/Explore';
+// @ts-ignore
+import { contextSrv } from 'grafana/app/core/core';
 
 export const Routes = () => {
+  const routes = [];
+  if (contextSrv.hasPermission('grafana-lokiexplore-app:read')) {
+    routes.push(<Route path={prefixRoute(ROUTES.Explore)} component={LogExplorationPage} />);
+    routes.push(<Redirect to={prefixRoute(ROUTES.Explore)} />);
+  }
+
   return (
     <Switch>
-      <Route path={prefixRoute(`${ROUTES.Explore}`)} component={LogExplorationPage} />
-      <Redirect to={prefixRoute(ROUTES.Explore)} />
+      {routes}
     </Switch>
   );
 };
