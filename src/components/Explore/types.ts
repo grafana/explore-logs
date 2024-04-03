8165b1646bdb14efab9405a052108f0df6cae957
Sven Grossmann <svennergr@gmail.com>
2024-04-03 16:12:30 +0200

    fix `/detected_labels` not being used

--- src/components/Explore/types.ts
+++ src/components/Explore/types.ts
@@ -2,3 +2,12 @@ export interface AppliedPattern {
   pattern: string;
   type: 'include' | 'exclude';
 }
+
+export type DetectedLabel = {
+  label: string;
+  cardinality: number;
+};
+
+export type DetectedLabelsResponse = {
+  detectedLabels: DetectedLabel[];
+};
