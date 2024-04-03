2a9dec8d403f1f468ef6aad79e262ff1d41cf894
Sven Grossmann <svennergr@gmail.com>
2024-04-03 09:48:12 +0200

    don't use core import

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -3,12 +3,13 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import { prefixRoute } from '../../utils/utils.routing';
 import { ROUTES } from '../../constants';
 import { LogExplorationPage } from '../../pages/Explore';
-// @ts-ignore
-import { contextSrv } from 'grafana/app/core/core';
+import { config } from '@grafana/runtime';
 
 export const Routes = () => {
+  const userPermissions = config.bootData.user.permissions;
+  const canUseApp = userPermissions?.['grafana-lokiexplore-app:read'] === true;
   const routes = [];
-  if (contextSrv.hasPermission('grafana-lokiexplore-app:read')) {
+  if (canUseApp) {
     routes.push(<Route path={prefixRoute(ROUTES.Explore)} component={LogExplorationPage} />);
     routes.push(<Redirect to={prefixRoute(ROUTES.Explore)} />);
   }
