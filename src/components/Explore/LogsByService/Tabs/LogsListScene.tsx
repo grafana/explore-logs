6677a798dcd53a0a8c028b0f71aec7c4d92fadbc
Galen <galen.kistler@grafana.com>
2024-04-04 14:48:15 -0500

    set min-height so logs panel is about as tall as the users viewport

--- src/components/Explore/LogsByService/Tabs/LogsListScene.tsx
+++ src/components/Explore/LogsByService/Tabs/LogsListScene.tsx
@@ -41,6 +41,7 @@ export class LogsListScene extends SceneObjectBase<LogsListSceneState> {
           ySizing: 'content'
         }),
         new SceneFlexItem({
+          minHeight: 'calc(100vh - 220px)',
           body: getLogsPanel(),
         }),
       ],
