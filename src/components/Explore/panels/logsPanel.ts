3c3e43c05f35daff1753251762e01ff684f1e1ee
Sven Grossmann <svennergr@gmail.com>
2024-04-04 12:05:54 +0200

    formatting

--- src/components/Explore/panels/logsPanel.ts
+++ src/components/Explore/panels/logsPanel.ts
@@ -1,8 +1,5 @@
 import { PanelBuilders } from '@grafana/scenes';
 
 export function getLogsPanel() {
-  return PanelBuilders.logs() //
-    .setTitle('Logs')
-    .setOption('showLogContextToggle', true)
-    .build();
+  return PanelBuilders.logs().setTitle('Logs').setOption('showLogContextToggle', true).build();
 }
