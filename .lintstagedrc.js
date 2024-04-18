3842a0197ca7d4914c03fed1435a87da4a212e19
L2D2Grafana <114438185+L2D2Grafana@users.noreply.github.com>
2024-04-18 13:59:30 -0700

    chore: commit lint, prettier

---
+++ .lintstagedrc.js
@@ -0,0 +1,6 @@
+const PRETTIER_WRITE = 'prettier --write';
+
+module.exports = {
+  '**/!(package).json': [PRETTIER_WRITE],
+  'src/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'tsc-files --noEmit', PRETTIER_WRITE],
+};
