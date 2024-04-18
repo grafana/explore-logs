107cd23a0912595ede4c14866737a75fc617f0d2
Galen <galen.kistler@grafana.com>
2024-04-18 15:30:12 -0500

    filter by cardinality

--- src/components/Tabs/DetectedFields/FieldsBreakdownScene.tsx
+++ src/components/Tabs/DetectedFields/FieldsBreakdownScene.tsx
@@ -95,15 +95,18 @@ export class FieldsBreakdownScene extends SceneObjectBase<FieldsBreakdownSceneSt
   private updateFields() {
     const variable = this.getVariable();
     const logsScene = sceneGraph.getAncestor(this, ByServiceScene);
-
-    this.setState({
-      fields: [
-        { label: 'All', value: ALL_VARIABLE_VALUE },
-        ...(logsScene.state.detectedFields?.map((f) => ({
+    const fields = [
+      { label: 'All', value: ALL_VARIABLE_VALUE },
+      ...(logsScene.state.detectedFields
+        ?.filter((f) => f.cardinality > 1)
+        .map((f) => ({
           label: f.label,
           value: f.label,
         })) || []),
-      ],
+    ];
+
+    this.setState({
+      fields,
     });
 
     this.updateBody(variable);
