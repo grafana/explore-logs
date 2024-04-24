b17a6cbd8636464215543afe65abb6ed84a473e4
L2D2Grafana <114438185+L2D2Grafana@users.noreply.github.com>
2024-04-24 08:56:55 -0700

    feat: volume 404 add theme test

--- src/Components/GrotError.test.tsx
+++ src/Components/GrotError.test.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { GrotError } from './GrotError';
 
+jest.mock('@grafana/ui', () => ({
+  ...jest.requireActual('@grafana/ui'),
+  useTheme2: jest.fn(() => ({
+    isDark: false,
+  })),
+}));
+
 describe('GrotError', () => {
+  it('renders the grot light error graphic when isDark false', () => {
+    const { getByTestId } = render(<GrotError>Log volume has not been configured.</GrotError>);
+
+    const graphic = getByTestId('grot_err_light');
+
+    expect(graphic).toBeInTheDocument();
+  });
+
   it('renders the error graphic and message', () => {
     const { getByText, getByTestId } = render(<GrotError>Log volume has not been configured.</GrotError>);
 
-    const graphic = getByTestId('grot_err');
+    const graphic = getByTestId('grot_err_light');
     const message = getByText('Log volume has not been configured.');
 
     expect(graphic).toBeInTheDocument();
