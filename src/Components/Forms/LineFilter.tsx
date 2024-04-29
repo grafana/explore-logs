d5999f256f5cb2ea299525494c43c8707310cadf
Liza Detrick <114438185+L2D2Grafana@users.noreply.github.com>
2024-04-29 09:38:13 -0700

    chore: add unit and e2e tests to ci, scaffold playwright (#259)
    * chore: add playwright jest in ci

--- src/Components/Forms/LineFilter.tsx
+++ src/Components/Forms/LineFilter.tsx
@@ -4,7 +4,7 @@ import { Field, Input } from '@grafana/ui';
 import { debounce } from 'lodash';
 import React, { ChangeEvent } from 'react';
 import { VAR_LINE_FILTER } from 'services/shared';
-
+import { testIds } from 'Components/testIds';
 interface LineFilterState extends SceneObjectState {
   lineFilter: string;
 }
@@ -57,7 +57,13 @@ function LineFilterRenderer({ model }: SceneComponentProps<LineFilter>) {
 
   return (
     <Field>
-      <Input value={lineFilter} className={styles.input} onChange={model.handleChange} placeholder="Search" />
+      <Input
+        data-testid={testIds.exploreServiceBreakdown.search}
+        value={lineFilter}
+        className={styles.input}
+        onChange={model.handleChange}
+        placeholder="Search"
+      />
     </Field>
   );
 }
---
+++ tests/fixtures/explore.ts
@@ -0,0 +1,34 @@
+import type { Page, Locator } from '@playwright/test';
+import pluginJson from '../../src/plugin.json';
+import { ROUTES } from '../../src/services/routing';
+import { testIds } from '../../src/Components/testIds';
+
+export class ExplorePage {
+  private readonly firstServicePageSelect: Locator;
+  logVolumeGraph: Locator;
+  servicesSearch: Locator;
+  serviceBreakdownSearch: Locator;
+  serviceBreakdownOpenExplore: Locator;
+
+  constructor(public readonly page: Page) {
+    this.firstServicePageSelect = this.page.getByText('Select').first();
+    this.logVolumeGraph = this.page.getByText('Log volume');
+    this.servicesSearch = this.page.getByTestId(testIds.exploreService.search);
+    this.serviceBreakdownSearch = this.page.getByTestId(testIds.exploreServiceBreakdown.search);
+    this.serviceBreakdownOpenExplore = this.page.getByTestId(testIds.exploreServiceBreakdown.openExplore);
+  }
+
+  async gotoServices() {
+    await this.page.goto(`/a/${pluginJson.id}/${ROUTES.Explore}`);
+  }
+
+  async addServiceName() {
+    await this.firstServicePageSelect.click();
+  }
+
+  async gotoServicesBreakdown() {
+    await this.page.goto(
+      `/a/${pluginJson.id}/${ROUTES.Explore}?mode=logs&var-patterns=&var-filters=service_name%7C%3D%7Ctempo-distributor&actionView=logs&var-logsFormat=%20%7C%20logfmt`
+    );
+  }
+}
