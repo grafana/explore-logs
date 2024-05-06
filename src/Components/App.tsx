93681deb1aaade0ec3eb7710a2b85866a7ad5010
Galen <galen.kistler@grafana.com>
2024-05-06 13:50:25 -0500

    chore(table): init table functionality working from other branch

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { AppRootProps } from '@grafana/data';
 import { Routes } from './Routes';
+import { sceneUtils } from '@grafana/scenes';
+import { LOGS_TABLE_PLUGIN_ID } from './Table/tablePanel';
+import { customTablePanel } from './Table/registerTable';
 
-// sceneUtils.registerRuntimePanelPlugin({ pluginId: LOGS_TABLE_PLUGIN_ID, plugin: customTablePanel });
+// @todo where to register custom panels?
+sceneUtils.registerRuntimePanelPlugin({ pluginId: LOGS_TABLE_PLUGIN_ID, plugin: customTablePanel });
 
 export class App extends React.PureComponent<AppRootProps> {
   render() {
--- src/Components/ServiceScene/LogsListScene.tsx
+++ src/Components/ServiceScene/LogsListScene.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 
 import {
+  AdHocFiltersVariable,
   PanelBuilders,
   SceneComponentProps,
   SceneFlexItem,
   SceneFlexLayout,
+  sceneGraph,
   SceneObjectBase,
   SceneObjectState,
+  VizPanel,
 } from '@grafana/scenes';
 import { LineFilter } from './LineFilter';
+import { LOGS_TABLE_PLUGIN_ID, LogsPanelHeaderActions } from '../Table/tablePanel';
+import { AdHocVariableFilter } from '@grafana/data';
+import { VAR_FIELDS } from '../../services/variables';
 
 export interface LogsListSceneState extends SceneObjectState {
   loading?: boolean;
   panel?: SceneFlexLayout;
+  visualizationType: LogsVisualizationType;
 }
+
+export type LogsVisualizationType = 'logs' | 'table';
+// If we use the local storage key from explore the user will get more consistent UX?
+const VISUALIZATION_TYPE_LOCALSTORAGE_KEY = 'grafana.explore.logs.visualisationType';
 
 export class LogsListScene extends SceneObjectBase<LogsListSceneState> {
   constructor(state: Partial<LogsListSceneState>) {
     super({
       ...state,
+      visualizationType:
+        // (localStorage.getItem(VISUALIZATION_TYPE_LOCALSTORAGE_KEY) as LogsVisualizationType) ?? 'table',
+        'logs',
     });
 
     this.addActivationHandler(this._onActivate.bind(this));
@@ -30,6 +44,71 @@ export class LogsListScene extends SceneObjectBase<LogsListSceneState> {
         panel: this.getVizPanel(),
       });
     }
+    this._subs.add(
+      this.subscribeToState((newState, prevState) => {
+        if (prevState.visualizationType !== newState.visualizationType) {
+          this.setState({
+            panel: this.getVizPanel(),
+          });
+        }
+      })
+    );
+  }
+
+  private getTablePanel(): SceneFlexItem {
+    const fields = sceneGraph.lookupVariable(VAR_FIELDS, this)! as AdHocFiltersVariable;
+    const addFilter = (filter: AdHocVariableFilter) => {
+      const fields = sceneGraph.lookupVariable(VAR_FIELDS, this)! as AdHocFiltersVariable;
+      const filters = fields.state.filters;
+      fields.setState({
+        filters: [...filters, filter],
+      });
+    };
+
+    return new SceneFlexItem({
+      height: 'calc(100vh - 220px)',
+      body: new VizPanel({
+        pluginId: LOGS_TABLE_PLUGIN_ID,
+        options: {
+          filters: fields.state.filters,
+          addFilter,
+          // @todo selected line should be moved to table scene,
+          // @todo timerange should be moved to table scene
+        },
+        title: 'Logs',
+        headerActions: (
+          <LogsPanelHeaderActions
+            vizType={this.state.visualizationType}
+            onChange={this.setVisualizationType.bind(this)}
+          />
+        ),
+      }),
+    });
+  }
+
+  private getLogsPanel() {
+    return new SceneFlexItem({
+      height: 'calc(100vh - 220px)',
+      body: PanelBuilders.logs()
+        .setTitle('Logs')
+        .setOption('showLogContextToggle', true)
+        .setOption('showTime', true)
+        .setHeaderActions(
+          <LogsPanelHeaderActions
+            vizType={this.state.visualizationType}
+            onChange={this.setVisualizationType.bind(this)}
+          />
+        )
+        .build(),
+    });
+  }
+
+  private setVisualizationType(type: LogsVisualizationType) {
+    console.log('type', type);
+    this.setState({
+      visualizationType: type,
+    });
+    localStorage.setItem(VISUALIZATION_TYPE_LOCALSTORAGE_KEY, type);
   }
 
   private getVizPanel() {
@@ -40,14 +119,7 @@ export class LogsListScene extends SceneObjectBase<LogsListSceneState> {
           body: new LineFilter(),
           ySizing: 'content',
         }),
-        new SceneFlexItem({
-          height: 'calc(100vh - 220px)',
-          body: PanelBuilders.logs()
-            .setTitle('Logs')
-            .setOption('showLogContextToggle', true)
-            .setOption('showTime', true)
-            .build(),
-        }),
+        this.state.visualizationType === 'logs' ? this.getLogsPanel() : this.getTablePanel(),
       ],
     });
   }
