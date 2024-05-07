1fee26475878e2f9c42e38fd7ae78a17e757fc93
Matias Chomicki <matyax@gmail.com>
2024-05-07 17:25:15 +0200

    test(fields): create unit test

---
+++ src/services/fields.test.ts
@@ -0,0 +1,25 @@
+import { FieldType, createDataFrame } from '@grafana/data';
+
+import { extractParserAndFieldsFromDataFrame } from './fields';
+
+describe('', () => {
+  const dataFrame = createDataFrame({
+    refId: 'A',
+    fields: [
+      { name: 'Time', type: FieldType.time, values: [0] },
+      {
+        name: 'Line',
+        type: FieldType.string,
+        values: ['line1'],
+      },
+      { name: 'labelTypes', type: FieldType.other, values: [{ field1: 'I', field2: 'P', field3: 'S' }] },
+    ],
+  });
+
+  test('Extracts parser and fields from a data frame', () => {
+    expect(extractParserAndFieldsFromDataFrame(dataFrame)).toEqual({
+      type: 'logfmt',
+      fields: ['field2'],
+    });
+  });
+});
