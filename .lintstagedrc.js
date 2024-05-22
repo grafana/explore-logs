2d41720b9ee7d8fe8efcfa8a234a5d06c489c1c0
Galen <galen.kistler@grafana.com>
2024-05-22 10:52:03 -0500

    chore(prettier): trying to run on test files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,6 @@ const PRETTIER_WRITE = 'prettier --write';
 
 module.exports = {
   '**/!(package).json': [PRETTIER_WRITE],
+  'tests/**/*.{ts}': [PRETTIER_WRITE],
   'src/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'tsc-files --noEmit', PRETTIER_WRITE],
-  'tests/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'tsc-files --noEmit', PRETTIER_WRITE],
 };
