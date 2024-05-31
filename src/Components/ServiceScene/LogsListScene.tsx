7416cd4d7c04b95fb14f073269b8d5df960a0a09
Sven Grossmann <svennergr@gmail.com>
2024-05-31 13:04:38 +0200

    feat(move-log-volume): moves log volume panel inside logs tab

--- src/Components/ServiceScene/LogsListScene.tsx
+++ src/Components/ServiceScene/LogsListScene.tsx
@@ -9,6 +9,7 @@ import {
   SceneObjectState,
 } from '@grafana/scenes';
 import { LineFilter } from './LineFilter';
+import { LogsVolumePanel } from './LogsVolumePanel';
 
 export interface LogsListSceneState extends SceneObjectState {
   loading?: boolean;
@@ -64,7 +65,16 @@ export class LogsListScene extends SceneObjectBase<LogsListSceneState> {
 }
 
 export function buildLogsListScene() {
-  return new SceneFlexItem({
-    body: new LogsListScene({}),
+  return new SceneFlexLayout({
+    direction: 'column',
+    children: [
+      new SceneFlexItem({
+        height: 200,
+        body: new LogsVolumePanel({}),
+      }),
+      new SceneFlexItem({
+        body: new LogsListScene({}),
+      }),
+    ],
   });
 }
