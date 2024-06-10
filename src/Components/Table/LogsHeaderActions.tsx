25000cad23635dc12816cc62b4771b48cdd38626
Galen <galen.kistler@grafana.com>
2024-06-10 13:42:55 -0500

    chore(table): refactor

--- src/Components/Table/LogsHeaderActions.tsx
+++ src/Components/Table/LogsHeaderActions.tsx
@@ -3,14 +3,10 @@ import { RadioButtonGroup } from '@grafana/ui';
 import { css } from '@emotion/css';
 import { LogsVisualizationType } from '../ServiceScene/LogsListScene';
 
-export const LOGS_TABLE_PLUGIN_ID = 'logs-table';
-
 export function LogsPanelHeaderActions(props: {
   vizType: LogsVisualizationType;
   onChange: (type: LogsVisualizationType) => void;
 }) {
-  const styles = getStyles();
-
   return (
     <div className={styles.visualisationType}>
       <RadioButtonGroup
@@ -35,16 +31,14 @@ export function LogsPanelHeaderActions(props: {
   );
 }
 
-const getStyles = () => {
-  return {
-    visualisationType: css({
-      display: 'flex',
-      flex: '1',
-      justifyContent: 'space-between',
-      marginTop: '8px',
-    }),
-    visualisationTypeRadio: css({
-      margin: `0 0 0 8px`,
-    }),
-  };
+const styles = {
+  visualisationType: css({
+    display: 'flex',
+    flex: '1',
+    justifyContent: 'space-between',
+    marginTop: '8px',
+  }),
+  visualisationTypeRadio: css({
+    margin: `0 0 0 8px`,
+  }),
 };
