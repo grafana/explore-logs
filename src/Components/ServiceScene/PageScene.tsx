0d94cb891072f52baa29cd6a2fc9cc4417d2319d
Galen <galen.kistler@grafana.com>
2024-06-11 14:01:18 -0500

    feat(breadcrumbs): add service name to breadcrumb as parent of children pages

--- src/Components/ServiceScene/PageScene.tsx
+++ src/Components/ServiceScene/PageScene.tsx
@@ -1,7 +1,16 @@
-import { SceneComponentProps, SceneObject, SceneObjectBase, SceneObjectState } from '@grafana/scenes';
+import {
+  AdHocFiltersVariable,
+  SceneComponentProps,
+  sceneGraph,
+  SceneObject,
+  SceneObjectBase,
+  SceneObjectState,
+} from '@grafana/scenes';
 import { PageLayoutType } from '@grafana/data';
 import { PluginPage } from '@grafana/runtime';
 import React from 'react';
+import { VAR_FILTERS } from '../../services/variables';
+import { prefixRoute, ROUTES } from '../../services/routing';
 
 interface PageSceneState extends SceneObjectState {
   body: SceneObject;
@@ -16,8 +25,26 @@ export class PageScene extends SceneObjectBase<PageSceneState> {
   }
   public static Component = ({ model }: SceneComponentProps<PageScene>) => {
     const { body, title } = model.useState();
+    const vars = sceneGraph.lookupVariable(VAR_FILTERS, model) as AdHocFiltersVariable;
+    const { filters } = vars.useState();
+    const serviceName = filters?.[0].value;
+
+    const parentNav = serviceName
+      ? {
+          text: serviceName,
+          // Core will dedupe breadcrumbs with the same url, appending an anchor link so we can get the service name, even though it links to same location as "Logs" breadcrumb
+          url: prefixRoute(ROUTES.Explore) + '#',
+        }
+      : undefined;
+
     return (
-      <PluginPage pageNav={{ text: title }} layout={PageLayoutType.Custom}>
+      <PluginPage
+        pageNav={{
+          text: title,
+          parentItem: parentNav,
+        }}
+        layout={PageLayoutType.Custom}
+      >
         <body.Component model={body} />
       </PluginPage>
     );
