f176fe60eb65cb4b594a63fd88aac403e21dd4a9
Matias Chomicki <matyax@gmail.com>
2024-06-14 14:52:12 +0200

    chore: explain source of the type

--- src/services/fields.ts
+++ src/services/fields.ts
@@ -68,6 +68,7 @@ export function getFilterBreakdownValueScene(
   };
 }
 
+// copied from public/app/plugins/datasource/loki/types.ts
 export enum LabelType {
   Indexed = 'I',
   StructuredMetadata = 'S',
