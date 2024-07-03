602d3dc01bd2713b2302654f13c817d4fd3ef64b
Matias Chomicki <matyax@gmail.com>
2024-07-03 16:13:37 +0200

    chore: install and configure augurs

---
+++ webpack.config.ts
@@ -0,0 +1,15 @@
+import type { Configuration } from 'webpack';
+import { merge } from 'webpack-merge';
+import grafanaConfig from './.config/webpack/webpack.config';
+
+const config = async (env): Promise<Configuration> => {
+  const baseConfig = await grafanaConfig(env);
+  return merge(baseConfig, {
+    experiments: {
+      // Required to load WASM modules.
+      asyncWebAssembly: true,
+    },
+  });
+};
+
+export default config;
