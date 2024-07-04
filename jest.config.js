61804f0c5399432d9e848278249b806f6c981d89
Matias Chomicki <matyax@gmail.com>
2024-07-04 19:03:26 +0200

    chore(jest): configure module mapper and ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const { grafanaESModules, nodeModulesToTransform } = require('./.config/jest/utils');
+
 // force timezone to UTC to allow tests to work regardless of local timezone
 // generally used by snapshots, but can affect specific tests
 process.env.TZ = 'UTC';
@@ -5,5 +7,8 @@ process.env.TZ = 'UTC';
 module.exports = {
   // Jest configuration provided by Grafana scaffolding
   ...require('./.config/jest.config'),
-  moduleDirectories: ['public', 'node_modules'],
+  moduleNameMapper: {
+    '@bsull/augurs': '@bsull/augurs/augurs.js',
+  },
+  transformIgnorePatterns: [nodeModulesToTransform([...grafanaESModules, '@bsull/augurs'])],
 };
