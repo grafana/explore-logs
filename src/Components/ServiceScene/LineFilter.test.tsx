cefdb2b772ce30c840bef1bf3ab5ec2e3900ced7
Matias Chomicki <matyax@gmail.com>
2024-07-09 17:16:28 +0200

    test(LineFilter): fix test

--- src/Components/ServiceScene/LineFilter.test.tsx
+++ src/Components/ServiceScene/LineFilter.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { LineFilter } from './LineFilter';
 import userEvent from '@testing-library/user-event';
 import { CustomVariable, SceneVariableSet } from '@grafana/scenes';
@@ -26,7 +26,7 @@ describe('LineFilter', () => {
   test('Updates the variable with the user input', async () => {
     render(<scene.Component model={scene} />);
 
-    await userEvent.type(screen.getByPlaceholderText('Search in log lines'), 'some text');
+    await act(() => userEvent.type(screen.getByPlaceholderText('Search in log lines'), 'some text'));
 
     expect(await screen.findByDisplayValue('some text')).toBeInTheDocument();
     expect(lineFilterVariable.getValue()).toBe('|~ `(?i)some text`');
@@ -35,7 +35,7 @@ describe('LineFilter', () => {
   test('Escapes the regular expression in the variable', async () => {
     render(<scene.Component model={scene} />);
 
-    await userEvent.type(screen.getByPlaceholderText('Search in log lines'), '(characters');
+    await act(() => userEvent.type(screen.getByPlaceholderText('Search in log lines'), '(characters'));
 
     expect(await screen.findByDisplayValue('(characters')).toBeInTheDocument();
     expect(lineFilterVariable.getValue()).toBe('|~ `(?i)\\(characters`');
