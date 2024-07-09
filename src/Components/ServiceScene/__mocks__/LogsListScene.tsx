bfd4682c33ecf987679358102cb222922fc92b0f
Matias Chomicki <matyax@gmail.com>
2024-07-09 17:40:49 +0200

    Logs panel: implement wrap lines option (#539)
    * feat(LogOptions): create log options component
    
    * fix(LineFilter): make label 100% width
    
    * feat(LogListScene): integrate LogOptionsScene
    
    * feat(LogOptions): re-render logs panel when the setting changes
    
    * chore(LineFilter): rename to LineFilterScene
    
    * test(LogOptionsScene): add unit test

---
+++ src/Components/ServiceScene/__mocks__/LogsListScene.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { SceneComponentProps, SceneFlexLayout, SceneObjectBase } from '@grafana/scenes';
+import { LogsListSceneState } from '../LogsListScene';
+import { LogOptionsScene } from '../LogOptionsScene';
+
+export class LogsListScene extends SceneObjectBase<LogsListSceneState> {
+  constructor(state: Partial<LogsListSceneState>) {
+    super({
+      ...state,
+      visualizationType: 'logs',
+      panel: new SceneFlexLayout({
+        children: [new LogOptionsScene()],
+      }),
+    });
+  }
+
+  public updateLogsPanel = jest.fn();
+
+  public static Component = ({ model }: SceneComponentProps<LogsListScene>) => {
+    const { panel } = model.useState();
+    if (!panel) {
+      return null;
+    }
+    return (
+      <div>
+        <panel.Component model={panel} />
+      </div>
+    );
+  };
+}
