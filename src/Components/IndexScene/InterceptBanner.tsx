8024b0c4bd0083a79018356da43cec9a57fc2e54
Galen <galen.kistler@grafana.com>
2024-07-11 07:37:25 -0500

    chore: set link targets to _blank

--- src/Components/IndexScene/InterceptBanner.tsx
+++ src/Components/IndexScene/InterceptBanner.tsx
@@ -15,23 +15,32 @@ export function InterceptBanner(props: { onRemove: () => void }) {
           Check out our{' '}
           <a
             className="external-link"
+            target={'_blank'}
             href="https://grafana.com/docs/grafana-cloud/visualizations/simplified-exploration/logs/   "
+            rel="noreferrer"
           >
             Get started doc
           </a>
           , or see{' '}
-          <a className={'external-link'} href="https://github.com/grafana/explore-logs/releases">
+          <a
+            className={'external-link'}
+            target={'_blank'}
+            href="https://github.com/grafana/explore-logs/releases"
+            rel="noreferrer"
+          >
             recent changes
           </a>
           .<br />
           Help us shape the future of the app.{' '}
-          <a className={'external-link'} href="https://forms.gle/1sYWCTPvD72T1dPH9">
+          <a className={'external-link'} target={'_blank'} href="https://forms.gle/1sYWCTPvD72T1dPH9" rel="noreferrer">
             Send us feedback
           </a>{' '}
           or engage with us on{' '}
           <a
             className={'external-link'}
+            target={'_blank'}
             href="https://github.com/grafana/explore-logs/?tab=readme-ov-file#explore-logs"
+            rel="noreferrer"
           >
             GitHub
           </a>
