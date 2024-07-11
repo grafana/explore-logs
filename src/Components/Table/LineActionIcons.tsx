925a84bae1252d0201d23a1ca79f2f90197b53d9
Galen Kistler <109082771+gtk-grafana@users.noreply.github.com>
2024-07-11 07:44:01 -0500

    Table: Remove link to log line feature (#571)
    * chore: remove link to log line, revert me after fixing scrollTo bug
    
    ---------
    
    Co-authored-by: Sven Grossmann <Svennergr@gmail.com>

--- src/Components/Table/LineActionIcons.tsx
+++ src/Components/Table/LineActionIcons.tsx
@@ -2,10 +2,8 @@ import { ClipboardButton, IconButton, Modal, useTheme2 } from '@grafana/ui';
 import React, { useState } from 'react';
 import { GrafanaTheme2 } from '@grafana/data';
 import { css } from '@emotion/css';
-import { SelectedTableRow } from 'Components/Table/LogLineCellComponent';
 import { useQueryContext } from 'Components/Table/Context/QueryContext';
 import { testIds } from '../../services/testIds';
-import { locationService } from '@grafana/runtime';
 
 export enum UrlParameterType {
   SelectedLine = 'selectedLine',
@@ -49,8 +47,7 @@ export const getStyles = (theme: GrafanaTheme2, bgColor?: string) => ({
 export function LineActionIcons(props: { rowIndex: number; value: unknown }) {
   const theme = useTheme2();
   const styles = getStyles(theme);
-  const { logsFrame, timeRange } = useQueryContext();
-  const logId = logsFrame?.idField?.values[props.rowIndex];
+  const { logsFrame } = useQueryContext();
   const lineValue = logsFrame?.bodyField.values[props.rowIndex];
   const [isInspecting, setIsInspecting] = useState(false);
   return (
@@ -68,38 +65,6 @@ export function LineActionIcons(props: { rowIndex: number; value: unknown }) {
             name="eye"
             onClick={() => setIsInspecting(true)}
             tabIndex={0}
-          />
-        </div>
-        <div className={styles.inspect}>
-          <ClipboardButton
-            className={styles.clipboardButton}
-            icon="share-alt"
-            variant="secondary"
-            fill="text"
-            size="md"
-            tooltip="Copy link to log line"
-            tooltipPlacement="top"
-            tabIndex={0}
-            getText={() => {
-              const location = locationService.getLocation();
-              const searchParams = new URLSearchParams(location.search);
-              if (searchParams && timeRange) {
-                const selectedLine: SelectedTableRow = {
-                  row: props.rowIndex,
-                  id: logId,
-                };
-
-                searchParams.set(UrlParameterType.From, timeRange.from.toISOString());
-                searchParams.set(UrlParameterType.To, timeRange.to.toISOString());
-                searchParams.set(UrlParameterType.SelectedLine, JSON.stringify(selectedLine));
-
-                // @todo can encoding + as %20 break other stuff? Can label names or values have + in them that we don't want encoded? Should we just update values?
-                // + encoding for whitespace is for application/x-www-form-urlencoded, which appears to be the default encoding for URLSearchParams, replacing + with %20 to keep urls meant for the browser from breaking
-                const searchString = searchParams.toString().replace(/\+/g, '%20');
-                return window.location.origin + location.pathname + '?' + searchString;
-              }
-              return '';
-            }}
           />
         </div>
       </div>
