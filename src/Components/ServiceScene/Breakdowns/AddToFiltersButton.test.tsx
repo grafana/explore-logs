414dec85fc65cfdc367d9a7777daeb4e90505d20
Galen <galen.kistler@grafana.com>
2024-07-16 12:49:44 -0500

    test: fix

--- src/Components/ServiceScene/Breakdowns/AddToFiltersButton.test.tsx
+++ src/Components/ServiceScene/Breakdowns/AddToFiltersButton.test.tsx
@@ -4,7 +4,7 @@ import { AddToFiltersButton, FilterType, addAdHocFilter, addToFilters } from './
 import { FieldType, createDataFrame } from '@grafana/data';
 import userEvent from '@testing-library/user-event';
 import { AdHocFiltersVariable, SceneObject, sceneGraph } from '@grafana/scenes';
-import { LEVEL_VARIABLE_VALUE, VAR_FIELDS, VAR_LABELS } from 'services/variables';
+import { LEVEL_VARIABLE_VALUE, VAR_FIELDS } from 'services/variables';
 
 describe('AddToFiltersButton', () => {
   it('updates correct variable passed to AddToFiltersButton', async () => {
@@ -150,7 +150,7 @@ describe('addToFilters and addAdHocFilter', () => {
       const lookupVariable = jest.spyOn(sceneGraph, 'lookupVariable').mockReturnValue(adHocVariable);
       addToFilters(LEVEL_VARIABLE_VALUE, 'info', 'include', {} as SceneObject, VAR_FIELDS);
 
-      expect(lookupVariable).toHaveBeenCalledWith(VAR_LABELS, expect.anything());
+      expect(lookupVariable).toHaveBeenCalledWith(VAR_FIELDS, expect.anything());
     });
   });
 
