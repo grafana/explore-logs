45b27896ab871c72041318e5ce2d38998e095ced
Sven Grossmann <Svennergr@gmail.com>
2024-07-24 15:17:56 +0200

    Label queries: remove unneccessary filters and parsers in query expression (#628)
    * feat(label-expression): move expression logic into app
    
    * feat(label-expression): add field filtering
    
    * feat(label-expression): add `unsub`
    
    * feat(label-expression): add type filter for array

--- src/services/scenes.ts
+++ src/services/scenes.ts
@@ -58,3 +58,7 @@ export function getAdHocFiltersVariable(variableName: string, sceneObject: Scene
   }
   return variable;
 }
+
+export function isDefined<T>(value: T | null | undefined): value is T {
+  return value !== null && value !== undefined;
+}
