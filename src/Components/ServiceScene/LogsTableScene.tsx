a34dc57280719d83d505872805cfbc117e44e977
Galen <galen.kistler@grafana.com>
2024-08-08 13:28:45 -0500

    fix: refactor and fix buggy re-rendering in logs panels

--- src/Components/ServiceScene/LogsTableScene.tsx
+++ src/Components/ServiceScene/LogsTableScene.tsx
@@ -1,6 +1,6 @@
-import { SceneComponentProps, sceneGraph, SceneObjectBase } from '@grafana/scenes';
+import { SceneComponentProps, SceneDataState, sceneGraph, SceneObjectBase, SceneObjectState } from '@grafana/scenes';
 import { LogsListScene } from './LogsListScene';
-import { AdHocVariableFilter } from '@grafana/data';
+import { AdHocVariableFilter, LoadingState } from '@grafana/data';
 import { TableProvider } from '../Table/TableProvider';
 import React, { useRef } from 'react';
 import { PanelChrome } from '@grafana/ui';
@@ -10,14 +10,50 @@ import { addAdHocFilter } from './Breakdowns/AddToFiltersButton';
 import { areArraysEqual } from '../../services/comparison';
 import { getLogsPanelFrame } from './ServiceScene';
 
-export class LogsTableScene extends SceneObjectBase {
+interface LogsTableSceneState extends SceneObjectState {
+  data?: SceneDataState;
+  loading?: LoadingState;
+}
+
+export class LogsTableScene extends SceneObjectBase<LogsTableSceneState> {
+  constructor(state: Partial<LogsTableSceneState>) {
+    super(state);
+
+    this.addActivationHandler(this.onActivate.bind(this));
+  }
+
+  /**
+   * We can't subscribe to the state of the data provider anymore, because there are multiple queries running in each data provider
+   * So we need to manually update the data state to prevent unnecessary re-renders that cause flickering and break loading states
+   */
+  public onActivate() {
+    this._subs.add(
+      sceneGraph.getData(this).subscribeToState((newState, prevState) => {
+        const dataFrame = getLogsPanelFrame(newState.data);
+
+        // Just this query is done
+        if (dataFrame) {
+          this.setState({
+            data: newState,
+            loading: newState.data?.state,
+          });
+        } else {
+          // Query is loading
+          this.setState({
+            loading: newState.data?.state,
+          });
+        }
+      })
+    );
+  }
   public static Component = ({ model }: SceneComponentProps<LogsTableScene>) => {
+    const styles = getStyles();
     // Get state from parent model
     const parentModel = sceneGraph.getAncestor(model, LogsListScene);
     const { selectedLine, urlColumns, visualizationType } = parentModel.useState();
 
-    // Get dataFrame
-    const { data } = sceneGraph.getData(model).useState();
+    // Get data state
+    const { data, loading } = model.useState();
 
     // Get time range
     const timeRange = sceneGraph.getTimeRange(model);
@@ -44,17 +80,16 @@ export class LogsTableScene extends SceneObjectBase {
       }
     };
 
-    const styles = getStyles();
-    const dataFrame = getLogsPanelFrame(data);
+    const dataFrame = getLogsPanelFrame(data?.data);
 
     return (
       <div className={styles.panelWrapper} ref={panelWrap}>
         <PanelChrome
-          loadingState={data?.state}
+          loadingState={loading}
           title={'Logs'}
           actions={<LogsPanelHeaderActions vizType={visualizationType} onChange={parentModel.setVisualizationType} />}
         >
-          {data?.series[0] && (
+          {dataFrame && (
             <TableProvider
               panelWrap={panelWrap}
               addFilter={addFilter}
