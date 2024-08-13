9a5f2ca02fe8a40a767df7926f11fc3cd79bc29f
Galen <galen.kistler@grafana.com>
2024-08-13 08:52:50 -0500

    chore: remove console.warn

--- src/Components/ServiceScene/Breakdowns/Patterns/PatternsBreakdownScene.tsx
+++ src/Components/ServiceScene/Breakdowns/Patterns/PatternsBreakdownScene.tsx
@@ -138,7 +138,6 @@ export class PatternsBreakdownScene extends SceneObjectBase<PatternsBreakdownSce
 
   private updatePatternFrames(lokiPatterns?: DataFrame[]) {
     if (!lokiPatterns) {
-      console.warn('failed to update pattern frames');
       return;
     }
 
