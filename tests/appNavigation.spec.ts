1a40254b064bc7967e6991fe57ecca2def7510bc
Galen <galen.kistler@grafana.com>
2024-08-14 08:00:34 -0500

    test: sort params

--- tests/appNavigation.spec.ts
+++ tests/appNavigation.spec.ts
@@ -15,10 +15,15 @@ test.describe('navigating app', () => {
     await expect(page.getByText('Data source')).toBeVisible();
   });
 
-  test('mega menu click should reset url params', async ({ page }) => {
+  test.only('mega menu click should reset url params', async ({ page }) => {
     await explorePage.gotoServicesBreakdown();
     await page.getByTestId('data-testid Toggle menu').click();
     await page.getByTestId('data-testid navigation mega-menu').getByRole('link', { name: 'Logs' }).click();
-    await expect(page).toHaveURL(/a\/grafana\-lokiexplore\-app\/explore\?patterns\=%5B%5D&var\-ds\=gdev\-loki&var\-patterns\=&var\-lineFilter\=&var\-logsFormat\=/);
+    await expect(page).toHaveURL(/a\/grafana\-lokiexplore\-app\/explore\?patterns\=%5B%5D/);
+    const actualSearchParams = new URLSearchParams(page.url())
+    const expectedSearchParams = new URLSearchParams('http://localhost:3001/a/grafana-lokiexplore-app/explore?patterns=%5B%5D&var-fields=&var-levels=&var-ds=gdev-loki&var-patterns=&var-lineFilter=&var-logsFormat=')
+    actualSearchParams.sort()
+    expectedSearchParams.sort()
+    expect(actualSearchParams.toString()).toEqual(expectedSearchParams.toString())
   });
 });
