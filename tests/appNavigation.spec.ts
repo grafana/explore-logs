d1caf7718fe27422f12452ecf75e57fea0fb12ff
Galen <galen.kistler@grafana.com>
2024-08-14 08:05:15 -0500

    test: remove only

--- tests/appNavigation.spec.ts
+++ tests/appNavigation.spec.ts
@@ -15,7 +15,7 @@ test.describe('navigating app', () => {
     await expect(page.getByText('Data source')).toBeVisible();
   });
 
-  test.only('mega menu click should reset url params', async ({ page }) => {
+  test('mega menu click should reset url params', async ({ page }) => {
     await explorePage.gotoServicesBreakdown();
     await page.getByTestId('data-testid Toggle menu').click();
     await page.getByTestId('data-testid navigation mega-menu').getByRole('link', { name: 'Logs' }).click();
