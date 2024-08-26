c0d63d3d833dcd421409fa03671991722596ffc8
Matias Chomicki <matyax@gmail.com>
2024-08-26 19:01:37 +0200

    chore: update mock

--- src/Components/ServiceScene/__mocks__/LogsListScene.tsx
+++ src/Components/ServiceScene/__mocks__/LogsListScene.tsx
@@ -12,6 +12,7 @@ export class LogsListScene extends SceneObjectBase<LogsListSceneState> {
       panel: new SceneFlexLayout({
         children: [new LogOptionsScene()],
       }),
+      displayedFields: [],
     });
   }
 
