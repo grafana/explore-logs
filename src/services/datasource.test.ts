bcb3850e9781c17622d434c21fe7290869985a2f
Galen <galen.kistler@grafana.com>
2024-09-03 10:13:19 -0500

    chore: update datasource detected_fields test

--- src/services/datasource.test.ts
+++ src/services/datasource.test.ts
@@ -1,5 +1,12 @@
 import { SceneDataQueryResourceRequest, WrappedLokiDatasource } from './datasource';
-import { DataFrame, DataQueryRequest, DataQueryResponse, DataSourcePluginMeta, dateTime } from '@grafana/data';
+import {
+  DataFrame,
+  DataQueryRequest,
+  DataQueryResponse,
+  DataSourcePluginMeta,
+  dateTime,
+  LoadingState,
+} from '@grafana/data';
 import { LokiQuery } from './query';
 import { Observable } from 'rxjs';
 import { DataSourceWithBackend } from '@grafana/runtime';
@@ -31,8 +38,9 @@ jest.mock('./scenes', () => ({
 }));
 describe('datasource', () => {
   describe('detected_fields', () => {
+    let detectedFieldsResponse: DetectedFieldsResponse;
     beforeEach(() => {
-      const detectedFieldsResponse = {
+      detectedFieldsResponse = {
         fields: [
           {
             label: 'caller',
@@ -85,11 +93,74 @@ describe('datasource', () => {
       const response = datasource.query(request) as Observable<DataQueryResponse>;
 
       response.subscribe((value) => {
+        if (value.state === LoadingState.Done) {
+          const dataFrame: DataFrame = value.data[0];
+          expect(dataFrame.fields[0].values).toEqual(['caller']);
+          expect(dataFrame.fields[1].values).toEqual([2]);
+          expect(dataFrame.fields[2].values).toEqual(['logfmt']);
+          done();
+        }
+      });
+    });
+    it('should strip out level_extracted and level', (done) => {
+      detectedFieldsResponse = {
+        fields: [
+          {
+            label: 'caller',
+            type: 'string',
+            cardinality: 2,
+            parsers: ['logfmt'],
+          },
+          {
+            label: 'level_extracted',
+            type: 'string',
+            cardinality: 4,
+            parsers: ['logfmt'],
+          },
+          {
+            label: 'level',
+            type: 'string',
+            cardinality: 4,
+            parsers: ['logfmt'],
+          },
+        ],
+      };
+      const datasource = new WrappedLokiDatasource('logs-explore', 'abc-123');
+      const request = {
+        app: 'logs-explore',
+        range: {
+          from: dateTime(),
+          to: dateTime(),
+          raw: {
+            from: dateTime(),
+            to: dateTime(),
+          },
+        },
+        scopedVars: {
+          __sceneObject: {},
+        },
+        targets: [
+          {
+            expr: '',
+            refId: '',
+            datasource: '',
+            resource: 'detected_fields',
+            queryType: '',
+            editorMode: 'code',
+            supportingQueryType: '',
+          },
+        ],
+      } as unknown as DataQueryRequest<LokiQuery & SceneDataQueryResourceRequest>;
+      const response = datasource.query(request) as Observable<DataQueryResponse>;
+
+      response.subscribe((value) => {
         const dataFrame: DataFrame = value.data[0];
-        expect(dataFrame.fields[0].values).toEqual(['caller']);
-        expect(dataFrame.fields[1].values).toEqual([2]);
-        expect(dataFrame.fields[2].values).toEqual(['logfmt']);
-        done();
+        if (value.state === LoadingState.Done) {
+          expect(dataFrame.fields[0].values).toEqual(['caller']);
+          expect(dataFrame.fields[1].values).toEqual([2]);
+          expect(dataFrame.fields[2].values).toEqual(['logfmt']);
+          done();
+        }
       });
     });
   });
