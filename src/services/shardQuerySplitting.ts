94c3a6e729c893d9b22c0cc776e50f456df6b783
Matias Chomicki <matyax@gmail.com>
2024-09-04 11:58:07 -0300

    chore(shardQuerySplitting): add docs

--- src/services/shardQuerySplitting.ts
+++ src/services/shardQuerySplitting.ts
@@ -13,35 +13,49 @@ import { combineResponses } from './combineResponses';
 import { DataSourceWithBackend } from '@grafana/runtime';
 
 /**
- * Based in the state of the current response, if any, adjust target parameters such as `maxLines`.
- * For `maxLines`, we will update it as `maxLines - current amount of lines`.
- * At the end, we will filter the targets that don't need to be executed in the next request batch,
- * because, for example, the `maxLines` have been reached.
+ * Query splitting by stream shards.
+ * Query splitting, in any version, was introduced in Loki to optimize querying for long intervals
+ * and high volume of data, dividing a big request into smaller sub-requests, combining and displaying
+ * the results as they arrive.
+ * This approach takes advantage of the __stream_shard__ internal label, representing how data is spread
+ * into different sources that can be queried individually.
+ *
+ * The main entry point of this module is runShardSplitQuery(), which prepares the query for execution and
+ * passes it to splitQueriesByStreamShard() to begin the querying loop.
+ *
+ * splitQueriesByStreamShard() has the following structure:
+ * - Creates and returns an Observable to which the UI will subscribe
+ * - Requests the __stream_shard__ values of the selected service:
+ *   . If there are no shard values, it falls back to the standard querying approach of the data source in runNonSplitRequest()
+ *   . If there are shards:
+ *     - It groups the shard requests in an array of arrays of shard numbers in groupShardRequests()
+ *     - It begins the querying loop with runNextRequest()
+ * - runNextRequest() will send a query using the nth (cycle) shard group, and has the following internal structure:
+ *   . adjustTargetsFromResponseState() will filter log queries targets that already received the requested maxLines
+ *   . interpolateShardingSelector() will update the stream selector with the current shard numbers
+ *   . After query execution:
+ *     - If the response is successful:
+ *       . It will add new data to the response with combineResponses()
+ *       . nextRequest() will use the current cycle and the total groups to determine the next request or complete execution with done()
+ *     - If the response is unsuccessful:
+ *       . If there are retry attempts, it will retry the current cycle, or else continue with the next cycle
+ *       . If the returned error is Maximum series reached, it will not retry
+ * - Once all request groups have been executed, it will be done()
  */
-function adjustTargetsFromResponseState(targets: LokiQuery[], response: DataQueryResponse | null): LokiQuery[] {
-  if (!response) {
-    return targets;
-  }
 
-  return targets
-    .map((target) => {
-      if (!target.maxLines || !isLogsQuery(target.expr)) {
-        return target;
-      }
-      const targetFrame = response.data.find((frame) => frame.refId === target.refId);
-      if (!targetFrame) {
-        return target;
-      }
-      const updatedMaxLines = target.maxLines - targetFrame.length;
-      return {
-        ...target,
-        maxLines: updatedMaxLines < 0 ? 0 : updatedMaxLines,
-      };
-    })
-    .filter((target) => target.maxLines === undefined || target.maxLines > 0);
+export function runShardSplitQuery(datasource: DataSourceWithBackend<LokiQuery>, request: DataQueryRequest<LokiQuery>) {
+  const queries = datasource
+    .interpolateVariablesInQueries(request.targets, request.scopedVars)
+    .filter((query) => query.expr)
+    .map((target) => ({
+      ...target,
+      expr: addShardingPlaceholderSelector(target.expr),
+    }));
+
+  return splitQueriesByStreamShard(datasource, request, queries);
 }
 
-export function splitQueriesByStreamShard(
+function splitQueriesByStreamShard(
   datasource: DataSourceWithBackend<LokiQuery>,
   request: DataQueryRequest<LokiQuery>,
   splittingTargets: LokiQuery[]
@@ -164,7 +178,7 @@ export function splitQueriesByStreamShard(
           console.warn(`Shard splitting not supported. Issuing a regular query.`);
           runNonSplitRequest(subscriber);
         } else {
-          const shardRequests = getShardRequests(shards, request.range);
+          const shardRequests = groupShardRequests(shards, request.range);
           console.log(`Querying ${shards.join(', ')} shards`);
           runNextRequest(subscriber, 0, shardRequests);
         }
@@ -185,19 +199,7 @@ export function splitQueriesByStreamShard(
   return response;
 }
 
-export function runShardSplitQuery(datasource: DataSourceWithBackend<LokiQuery>, request: DataQueryRequest<LokiQuery>) {
-  const queries = datasource
-    .interpolateVariablesInQueries(request.targets, request.scopedVars)
-    .filter((query) => query.expr)
-    .map((target) => ({
-      ...target,
-      expr: addShardingPlaceholderSelector(target.expr),
-    }));
-
-  return splitQueriesByStreamShard(datasource, request, queries);
-}
-
-function getShardRequests(shards: number[], range: TimeRange) {
+function groupShardRequests(shards: number[], range: TimeRange) {
   const hours = range.to.diff(range.from, 'hour');
 
   shards.sort((a, b) => a - b);
@@ -223,6 +225,49 @@ function getShardRequests(shards: number[], range: TimeRange) {
   return requests;
 }
 
+/**
+ * Simple approach to calculate a maximum amount of requests to send based on
+ * the available shards, preventing an excessive number of sub-requests per query.
+ * For example:
+ * Shards => Requests
+ *   1    =>   1
+ *   2    =>   1
+ *   4    =>   2
+ *   8    =>   3
+ *   16   =>   4
+ *   32   =>   6
+ *   64   =>   8
+ *   128  =>   12
+ */
 function calculateMaxRequests(shards: number) {
-  return Math.min(Math.ceil(Math.sqrt(shards)), shards - 1);
+  return Math.max(Math.min(Math.ceil(Math.sqrt(shards)), shards - 1), 1);
+}
+
+/**
+ * Based in the state of the current response, if any, adjust target parameters such as `maxLines`.
+ * For `maxLines`, we will update it as `maxLines - current amount of lines`.
+ * At the end, we will filter the targets that don't need to be executed in the next request batch,
+ * because, for example, the `maxLines` have been reached.
+ */
+function adjustTargetsFromResponseState(targets: LokiQuery[], response: DataQueryResponse | null): LokiQuery[] {
+  if (!response) {
+    return targets;
+  }
+
+  return targets
+    .map((target) => {
+      if (!target.maxLines || !isLogsQuery(target.expr)) {
+        return target;
+      }
+      const targetFrame = response.data.find((frame) => frame.refId === target.refId);
+      if (!targetFrame) {
+        return target;
+      }
+      const updatedMaxLines = target.maxLines - targetFrame.length;
+      return {
+        ...target,
+        maxLines: updatedMaxLines < 0 ? 0 : updatedMaxLines,
+      };
+    })
+    .filter((target) => target.maxLines === undefined || target.maxLines > 0);
 }
