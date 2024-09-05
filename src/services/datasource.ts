aa230cd4f6ae05f64c3a87f8eafb154f7cfd0ae6
Matias Chomicki <matyax@gmail.com>
2024-09-04 22:35:30 -0300

    test(shardQuerySplitting): enable feature and test e2e

--- src/services/datasource.ts
+++ src/services/datasource.ts
@@ -8,7 +8,7 @@ import {
   LoadingState,
   TestDataSourceResponse,
 } from '@grafana/data';
-import { config, DataSourceWithBackend, getDataSourceSrv } from '@grafana/runtime';
+import { DataSourceWithBackend, getDataSourceSrv } from '@grafana/runtime';
 import { RuntimeDataSource, SceneObject, sceneUtils } from '@grafana/scenes';
 import { DataQuery } from '@grafana/schema';
 import { Observable, Subscriber } from 'rxjs';
@@ -121,8 +121,7 @@ class WrappedLokiDatasource extends RuntimeDataSource<DataQuery> {
     ds: DataSourceWithBackend<LokiQuery>,
     subscriber: Subscriber<DataQueryResponse>
   ) {
-    // @ts-expect-error
-    const shardingEnabled = config.featureToggles.exploreLogsShardSplitting;
+    const shardingEnabled = true;
 
     // Query the datasource and return either observable or promise
     const dsResponse = isLogsRequest(request) || !shardingEnabled ? ds.query(request) : runShardSplitQuery(ds, request);
