71d6ee072219d0ad966c1f9415991cd11ed26164
Galen <galen.kistler@grafana.com>
2024-09-12 16:36:47 -0500

    chore: reduce module

--- src/module.tsx
+++ src/module.tsx
@@ -1,21 +1,23 @@
 import { lazy } from 'react';
 import { AppPlugin } from '@grafana/data';
-// import App from 'Components/App';
-import init from '@bsull/augurs';
-import { linkConfigs } from 'services/extensions/links';
-import initRuntimeDs from 'services/datasource';
-import { wasmSupported } from 'services/sorting';
 
-const App = lazy(() => import('Components/App'));
+const App = lazy(async () => {
+  const { wasmSupported } = await import('services/sorting');
+  const { linkConfigs } = await import('services/extensions/links');
+  const { default: initRuntimeDs } = await import('services/datasource');
+  const { default: init } = await import('@bsull/augurs');
 
-if (wasmSupported()) {
-  init();
-}
+  initRuntimeDs();
+
+  for (const linkConfig of linkConfigs) {
+    plugin.configureExtensionLink(linkConfig);
+  }
+
+  if (wasmSupported()) {
+    await init();
+  }
+
+  return import('Components/App');
+});
 
 export const plugin = new AppPlugin<{}>().setRootPage(App);
-
-for (const linkConfig of linkConfigs) {
-  plugin.configureExtensionLink(linkConfig);
-}
-
-initRuntimeDs();
