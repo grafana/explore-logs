d1a78f76889ce9bba346ae674803753e25d95ddf
Galen <galen.kistler@grafana.com>
2024-10-16 10:10:16 -0500

    chore: revert unintended changes from previous merge

--- tests/appNavigation.spec.ts
+++ tests/appNavigation.spec.ts
@@ -23,7 +23,7 @@ test.describe('navigating app', () => {
     await expect(page).toHaveURL(/var-primary_label=service_name/);
     const actualSearchParams = new URLSearchParams(page.url().split('?')[1]);
     const expectedSearchParams = new URLSearchParams(
-      '?patterns=%5B%5D&from=now-15m&to=now&var-ds=gdev-loki&var-filters=&var-fields=&var-levels=&var-patterns=&var-lineFilter=&refresh=&var-primary_label=service_name%7C%3D~%7C.%2B'
+      '?patterns=%5B%5D&from=now-15m&to=now&var-ds=gdev-loki&var-filters=&var-fields=&var-levels=&var-patterns=&var-lineFilter=&var-metadata=&refresh=&var-primary_label=service_name%7C%3D~%7C.%2B'
     );
     actualSearchParams.sort();
     expectedSearchParams.sort();
@@ -38,7 +38,7 @@ test.describe('navigating app', () => {
     await expect(page).toHaveURL(/a\/grafana\-lokiexplore\-app\/explore\?patterns\=%5B%5D/);
     const actualSearchParams = new URLSearchParams(page.url().split('?')[1]);
     const expectedSearchParams = new URLSearchParams(
-      '?patterns=%5B%5D&from=now-15m&to=now&var-ds=gdev-loki&var-filters=&var-fields=&var-levels=&var-patterns=&var-lineFilter=&refresh=&var-primary_label=service_name%7C%3D~%7C.%2B'
+      '?patterns=%5B%5D&from=now-15m&to=now&var-ds=gdev-loki&var-filters=&var-fields=&var-levels=&var-patterns=&var-lineFilter=&var-metadata=&refresh=&var-primary_label=service_name%7C%3D~%7C.%2B'
     );
     actualSearchParams.sort();
     expectedSearchParams.sort();
