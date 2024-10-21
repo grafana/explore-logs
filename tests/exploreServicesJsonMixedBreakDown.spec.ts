8aa8464bbaaa8162809baf82fd7a2a8a97c463c5
Galen <galen.kistler@grafana.com>
2024-10-21 09:00:46 -0500

    test: flakey number of pods in ci

--- tests/exploreServicesJsonMixedBreakDown.spec.ts
+++ tests/exploreServicesJsonMixedBreakDown.spec.ts
@@ -175,7 +175,7 @@ test.describe('explore nginx-json-mixed breakdown pages ', () => {
       .click();
     const allPanels = explorePage.getAllPanelsLocator();
     // We should have more than 1 panels
-    await expect.poll(() => allPanels.count()).toBeGreaterThan(4);
+    await expect.poll(() => allPanels.count()).toBeGreaterThanOrEqual(4);
     const actualCount = await allPanels.count();
     // Should have 2 queries by now
     expect(requests).toHaveLength(2);
