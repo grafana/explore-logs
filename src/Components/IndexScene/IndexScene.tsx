05217ef81351ece6dcac82affc3023bb9b0d5ee8
Galen <galen.kistler@grafana.com>
2024-10-22 14:56:51 -0500

    chore: upgrade to 11.3.0 to avoid compatability check failure

--- src/Components/IndexScene/IndexScene.tsx
+++ src/Components/IndexScene/IndexScene.tsx
@@ -87,7 +87,6 @@ export class IndexScene extends SceneObjectBase<IndexSceneState> {
       new SceneRefreshPicker({}),
     ];
 
-    //@ts-expect-error
     if (getDrilldownSlug() === 'explore' && config.featureToggles.exploreLogsAggregatedMetrics) {
       controls.push(
         new ToolbarScene({
