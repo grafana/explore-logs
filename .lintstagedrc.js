719640d0edc04ed5aa9cb63a782de0385f0b79b5
Galen <galen.kistler@grafana.com>
2024-11-01 14:57:39 -0500

    chore: revert

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,6 @@ const PRETTIER_WRITE = 'prettier --write';
 
 module.exports = {
   '**/!(package).json': [PRETTIER_WRITE],
-  '*.{ts,tsx}': ['eslint --fix', 'bash -c tsc-files -p tsconfig.json --noEmit', PRETTIER_WRITE],
+  '*.{ts,tsx}': ['eslint --fix', 'bash -c tsc-files --noEmit', PRETTIER_WRITE],
   'tests/**/*.{js,jsx,ts,tsx}': ['eslint --fix', PRETTIER_WRITE],
 };
