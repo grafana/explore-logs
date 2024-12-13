cb23163084a1a3d84f9e9eaac435a977d75e8b02
Galen <galen.kistler@grafana.com>
2024-12-13 11:44:07 -0600

    chore: docs

--- src/services/keyboardShortcuts.ts
+++ src/services/keyboardShortcuts.ts
@@ -188,8 +188,12 @@ export class PasteTimeEvent extends BusEventWithPayload<PasteTimeEventPayload> {
 }
 
 /**
+ * Adds the scene object to the global window state so that templateSrv in core can interpolate strings using the scene interpolation engine with the scene as scope.
+ * This is needed for old datasources that call templateSrv.replace without passing scopedVars. For example in DataSourceAPI.metricFindQuery.
+ *
+ * This is also used from TimeSrv to access scene time range.
+ *
  * @todo delete after https://github.com/grafana/scenes/pull/999 is available
- * @param activeScene
  */
 export function setWindowGrafanaSceneContext(activeScene: SceneObject) {
   const prevScene = (window as any).__grafanaSceneContext;
