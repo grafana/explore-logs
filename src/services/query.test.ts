6d6dce662344afc4e7e4bc549d4384b0424b78a0
Galen <galen.kistler@grafana.com>
2025-01-17 12:38:15 -0600

    test: add test assertions

--- src/services/query.test.ts
+++ src/services/query.test.ts
@@ -5,6 +5,7 @@ import {
   renderLogQLFieldFilters,
   renderLogQLLabelFilters,
   renderLogQLLineFilter,
+  renderPatternFilters,
 } from './query';
 
 import { FieldValue } from './variables';
@@ -460,5 +461,54 @@ describe('joinTagFilters', () => {
 
     const result = joinTagFilters(adHoc);
     expect(result).toEqual(filters);
+  });
+});
+
+describe('renderPatternFilters', () => {
+  it('returns empty string if no patterns', () => {
+    expect(renderPatternFilters([])).toEqual('');
+  });
+  it('wraps in double quotes', () => {
+    expect(
+      renderPatternFilters([
+        {
+          pattern: 'level=info ts=<_> msg="completing block"',
+          type: 'include',
+        },
+      ])
+    ).toEqual(`|> "level=info ts=<_> msg=\\"completing block\\""`);
+  });
+  it('wraps in double quotes', () => {
+    expect(
+      renderPatternFilters([
+        {
+          pattern: 'level=info ts=<_> msg="completing block"',
+          type: 'include',
+        },
+      ])
+    ).toEqual(`|> "level=info ts=<_> msg=\\"completing block\\""`);
+  });
+  it('wraps in double quotes', () => {
+    expect(
+      renderPatternFilters([
+        {
+          pattern: 'level=info ts=<_> msg="completing block"',
+          type: 'include',
+        },
+      ])
+    ).toEqual(`|> "level=info ts=<_> msg=\\"completing block\\""`);
+  });
+  it('ignores backticks', () => {
+    expect(
+      renderPatternFilters([
+        {
+          pattern:
+            'logger=sqlstore.metrics traceID=<_> msg="query finished" sql="INSERT INTO instance (`org_id`, `result`) VALUES (?, ?) ON DUPLICATE KEY UPDATE `org_id`=VALUES(`org_id`)" error=null',
+          type: 'include',
+        },
+      ])
+    ).toEqual(
+      `|> "logger=sqlstore.metrics traceID=<_> msg=\\"query finished\\" sql=\\"INSERT INTO instance (\`org_id\`, \`result\`) VALUES (?, ?) ON DUPLICATE KEY UPDATE \`org_id\`=VALUES(\`org_id\`)\\" error=null"`
+    );
   });
 });
