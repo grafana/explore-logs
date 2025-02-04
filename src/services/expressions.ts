5f685a974bb2c12c5ad1846af7ffbe69638a56e5
Galen <galen.kistler@grafana.com>
2025-02-04 07:58:08 -0600

    chore: clean up & refactor

--- src/services/expressions.ts
+++ src/services/expressions.ts
@@ -1,10 +1,14 @@
 import {
+  DETECTED_FIELD_AND_METADATA_VALUES_EXPR,
+  DETECTED_LEVELS_VALUES_EXPR,
   JSON_FORMAT_EXPR,
   LEVEL_VARIABLE_VALUE,
   LOGS_FORMAT_EXPR,
   MIXED_FORMAT_EXPR,
+  VAR_FIELDS_AND_METADATA,
   VAR_FIELDS_EXPR,
   VAR_LABELS_EXPR,
+  VAR_LEVELS,
   VAR_LINE_FILTERS_EXPR,
   VAR_METADATA_EXPR,
   VAR_PATTERNS_EXPR,
@@ -12,6 +16,8 @@ import {
 import { SceneObject } from '@grafana/scenes';
 import { getParserFromFieldsFilters } from './fields';
 import { getFieldsVariable } from './variableGetters';
+import { UIVariableFilterType } from '../Components/ServiceScene/Breakdowns/AddToFiltersButton';
+import { logger } from './logger';
 
 /**
  * Crafts count over time query that excludes empty values for stream selector name
@@ -47,4 +53,24 @@ export function getTimeSeriesExpr(sceneRef: SceneObject, streamSelectorName: str
     }
   }
   return `sum(count_over_time({${VAR_LABELS_EXPR}} ${metadataExpressionToAdd} ${VAR_METADATA_EXPR} ${VAR_PATTERNS_EXPR} ${VAR_LINE_FILTERS_EXPR} ${VAR_FIELDS_EXPR} [$__auto])) by (${streamSelectorName})`;
+}
+
+/**
+ * Get expressions for UI variables
+ * @param variableType
+ */
+export function getFieldsTagValuesExpression(variableType: UIVariableFilterType) {
+  switch (variableType) {
+    case VAR_LEVELS:
+      return DETECTED_LEVELS_VALUES_EXPR;
+    case VAR_FIELDS_AND_METADATA:
+      return DETECTED_FIELD_AND_METADATA_VALUES_EXPR;
+    default:
+      const error = new Error(`Unknown variable type: ${variableType}`);
+      logger.error(error, {
+        variableType,
+        msg: `getFieldsTagValuesExpression: Unknown variable type: ${variableType}`,
+      });
+      throw error;
+  }
 }
