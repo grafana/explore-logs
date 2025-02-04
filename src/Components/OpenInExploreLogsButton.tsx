17d54dcd4c1309387d78a07b3cd0ad8b9ec904ed
kozhuhds <kozhuhds@gmail.com>
2025-02-04 18:26:36 +0100

    feat: exposing an open in explore logs link button

---
+++ src/Components/OpenInExploreLogsButton.tsx
@@ -0,0 +1,57 @@
+import { useReturnToPrevious } from '@grafana/runtime';
+import { LinkButton } from '@grafana/ui';
+import React, { useMemo } from 'react';
+
+export interface OpenInExploreLogsButtonProps {
+  datasourceUid?: string;
+  labelMatchers: Array<{ name: string; value: string }>;
+  from?: string;
+  to?: string;
+  returnToPreviousSource?: string;
+}
+
+export function OpenInExploreLogsButton({
+  datasourceUid,
+  labelMatchers,
+  from,
+  to,
+  returnToPreviousSource,
+}: OpenInExploreLogsButtonProps) {
+  const setReturnToPrevious = useReturnToPrevious();
+
+  const href = useMemo(() => {
+    const mainLabel = labelMatchers[0];
+
+    if (!mainLabel) {
+      return null;
+    }
+
+    const url = new URL(
+      `${window.location.origin}/a/grafana-lokiexplore-app/explore/${mainLabel.name}/${mainLabel.value}/logs`
+    );
+
+    datasourceUid && url.searchParams.set('var-datasource', datasourceUid);
+    from && url.searchParams.set('from', from);
+    to && url.searchParams.set('to', to);
+
+    labelMatchers.forEach((labelMatcher) => {
+      url.searchParams.append('var-filters', `${labelMatcher.name}|=|${labelMatcher.value}`);
+    });
+
+    return url.toString();
+  }, [datasourceUid, from, to, labelMatchers]);
+
+  if (!href) {
+    return null;
+  }
+
+  return (
+    <LinkButton
+      variant="secondary"
+      href={href}
+      onClick={() => setReturnToPrevious(returnToPreviousSource || 'previous')}
+    >
+      Open in Explore logs
+    </LinkButton>
+  );
+}
---
+++ src/services/extensions/exposedComponents.tsx
@@ -0,0 +1,10 @@
+import { OpenInExploreLogsButton } from 'Components/OpenInExploreLogsButton';
+
+export const exposedComponents = [
+  {
+    id: `grafana-lokiexplore-app/open-in-explore-logs-button/v1`,
+    title: 'Open in Explore logs button',
+    description: 'A button that opens a logs view in the Explore logs app.',
+    component: OpenInExploreLogsButton,
+  },
+];
