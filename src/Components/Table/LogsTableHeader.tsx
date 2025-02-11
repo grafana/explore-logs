032caead523e5dbd582096d3ddd9be2d0b1ba3b1
Galen <galen.kistler@grafana.com>
2025-02-11 17:49:58 -0600

    feat: remove pixel, log text and reset width column buttons

--- src/Components/Table/LogsTableHeader.tsx
+++ src/Components/Table/LogsTableHeader.tsx
@@ -2,11 +2,12 @@ import React, { PropsWithChildren, useRef } from 'react';
 import { css } from '@emotion/css';
 
 import { Field, GrafanaTheme2 } from '@grafana/data';
-import { ClickOutsideWrapper, Icon, Popover, useTheme2 } from '@grafana/ui';
+import { ClickOutsideWrapper, IconButton, Popover, useTheme2 } from '@grafana/ui';
 
 import { useTableHeaderContext } from 'Components/Table/Context/TableHeaderContext';
 import { useQueryContext } from './Context/QueryContext';
 import { getBodyName } from '../../services/logsFrame';
+import { LogLineState, useTableColumnContext } from './Context/TableColumnsContext';
 
 export interface LogsTableHeaderProps extends PropsWithChildren<CustomHeaderRendererProps> {
   fieldIndex: number;
@@ -18,6 +19,9 @@ export interface CustomHeaderRendererProps {
 }
 
 const getStyles = (theme: GrafanaTheme2, isFirstColumn: boolean, isLine: boolean) => ({
+  logLineButton: css({
+    marginLeft: '5px',
+  }),
   tableHeaderMenu: css({
     label: 'tableHeaderMenu',
     width: '100%',
@@ -31,14 +35,21 @@ const getStyles = (theme: GrafanaTheme2, isFirstColumn: boolean, isLine: boolean
     boxShadow: theme.shadows.z3,
     borderRadius: theme.shape.radius.default,
   }),
-  button: css({
-    appearance: 'none',
-    right: '5px',
-    background: 'none',
-    border: 'none',
-    padding: 0,
+  leftAlign: css({
+    label: 'left-align',
+    display: 'flex',
+    width: 'calc(100% - 20px)',
+  }),
+  clearButton: css({
+    marginLeft: '5px',
+  }),
+  rightAlign: css({
+    label: 'right-align',
+    display: 'flex',
+    marginRight: '5px',
   }),
   wrapper: css({
+    label: 'wrapper',
     display: 'flex',
     marginLeft: isFirstColumn ? '56px' : '6px',
     // Body has extra padding then other columns
@@ -62,19 +73,74 @@ export const LogsTableHeader = (props: LogsTableHeaderProps) => {
   const referenceElement = useRef<HTMLButtonElement | null>(null);
   const theme = useTheme2();
   const styles = getStyles(theme, props.fieldIndex === 0, props.field.name === getBodyName(logsFrame));
+  const { columnWidthMap, setColumnWidthMap } = useTableColumnContext();
+  const { setBodyState, bodyState } = useTableColumnContext();
+  const isBodyField = props.field.name === getBodyName(logsFrame);
+
+  const onLogTextToggle = () => {
+    if (bodyState === LogLineState.text) {
+      setBodyState(LogLineState.labels);
+    } else {
+      setBodyState(LogLineState.text);
+    }
+  };
 
   return (
     <span className={styles.wrapper}>
-      <span className={styles.defaultContentWrapper}>{props.defaultContent}</span>
-      <button
-        className={styles.button}
-        ref={referenceElement}
-        onClick={(e) => {
-          setHeaderMenuActive(!isHeaderMenuActive);
-        }}
-      >
-        <Icon title={'Show menu'} name={'ellipsis-v'} />
-      </button>
+      <span className={styles.leftAlign}>
+        <span className={styles.defaultContentWrapper}>{props.defaultContent}</span>
+        {columnWidthMap && setColumnWidthMap && columnWidthMap?.[props.field.name] !== undefined && (
+          <IconButton
+            tooltip={'Reset column width'}
+            tooltipPlacement={'top'}
+            className={styles.clearButton}
+            aria-label={'Reset column width'}
+            name={'x'}
+            onClick={() => {
+              const { [props.field.name]: omit, ...map } = { ...columnWidthMap };
+              setColumnWidthMap?.(map);
+            }}
+          />
+        )}
+        {isBodyField && (
+          <>
+            {bodyState === LogLineState.text ? (
+              <IconButton
+                tooltipPlacement={'top'}
+                tooltip={'Show log labels'}
+                aria-label={'Show log labels'}
+                onClick={onLogTextToggle}
+                className={styles.logLineButton}
+                name={'brackets-curly'}
+                size={'md'}
+              />
+            ) : (
+              <IconButton
+                tooltipPlacement={'top'}
+                tooltip={'Show log text'}
+                aria-label={'Show log text'}
+                onClick={onLogTextToggle}
+                className={styles.logLineButton}
+                name={'text-fields'}
+                size={'md'}
+              />
+            )}
+          </>
+        )}
+      </span>
+      <span className={styles.rightAlign}>
+        <IconButton
+          tooltip={`Show ${props.field.name} menu`}
+          tooltipPlacement={'top'}
+          ref={referenceElement}
+          aria-label={`Show ${props.field.name} menu`}
+          onClick={(e) => {
+            setHeaderMenuActive(!isHeaderMenuActive);
+          }}
+          name={'ellipsis-v'}
+        />
+      </span>
+
       {referenceElement.current && (
         //@ts-ignore
         <Popover
