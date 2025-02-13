b76afa6706652682234219427d345167e9f9d951
Dzmitry Kozhukh <kozhuhds@gmail.com>
2025-02-13 20:58:52 +0100

    feat: open in Explore logs button (#1035)
    * feat: exposing an open in explore logs link button

---
+++ src/Components/OpenInExploreLogsButton/OpenInExploreLogsButton.tsx
@@ -0,0 +1,92 @@
+import { useReturnToPrevious } from '@grafana/runtime';
+import { LinkButton } from '@grafana/ui';
+import React, { useMemo } from 'react';
+import {
+  appendUrlParameter,
+  createAppUrl,
+  escapeURLDelimiters,
+  replaceEscapeChars,
+  replaceSlash,
+  setUrlParameter,
+  stringifyAdHocValues,
+  UrlParameters,
+} from 'services/extensions/links';
+import { OpenInExploreLogsButtonProps } from './types';
+import { AbstractLabelOperator } from '@grafana/data';
+import { LabelFilterOp } from 'services/filterTypes';
+
+const operatorMap = {
+  [AbstractLabelOperator.Equal]: LabelFilterOp.Equal,
+  [AbstractLabelOperator.NotEqual]: LabelFilterOp.NotEqual,
+  [AbstractLabelOperator.EqualRegEx]: LabelFilterOp.RegexEqual,
+  [AbstractLabelOperator.NotEqualRegEx]: LabelFilterOp.RegexNotEqual,
+};
+
+export default function OpenInExploreLogsButton({
+  datasourceUid,
+  streamSelectors,
+  from,
+  to,
+  returnToPreviousSource,
+  renderButton,
+}: OpenInExploreLogsButtonProps) {
+  const setReturnToPrevious = useReturnToPrevious();
+
+  const href = useMemo(() => {
+    const mainLabel = streamSelectors[0];
+
+    if (
+      !mainLabel ||
+      // we can't open in explore logs if main label matcher is something different from equal
+      mainLabel?.operator !== AbstractLabelOperator.Equal
+    ) {
+      return null;
+    }
+
+    const labelValue = replaceSlash(mainLabel.value);
+
+    let params = new URLSearchParams();
+
+    if (datasourceUid) {
+      params = setUrlParameter(UrlParameters.DatasourceId, datasourceUid, params);
+    }
+
+    if (from) {
+      params = setUrlParameter(UrlParameters.TimeRangeFrom, from, params);
+    }
+
+    if (to) {
+      params = setUrlParameter(UrlParameters.TimeRangeTo, to, params);
+    }
+
+    streamSelectors.forEach((streamSelector) => {
+      params = appendUrlParameter(
+        UrlParameters.Labels,
+        `${streamSelector.name}|${operatorMap[streamSelector.operator]}|${escapeURLDelimiters(
+          stringifyAdHocValues(streamSelector.value)
+        )},${escapeURLDelimiters(replaceEscapeChars(streamSelector.value))}`,
+        params
+      );
+    });
+
+    return createAppUrl(`/explore/${mainLabel.name}/${labelValue}/logs`, params);
+  }, [datasourceUid, from, to, streamSelectors]);
+
+  if (!href) {
+    return null;
+  }
+
+  if (renderButton) {
+    return renderButton({ href });
+  }
+
+  return (
+    <LinkButton
+      variant="secondary"
+      href={href}
+      onClick={() => setReturnToPrevious(returnToPreviousSource || 'previous')}
+    >
+      Open in Explore Logs
+    </LinkButton>
+  );
+}
--- src/services/extensions/links.test.ts
+++ src/services/extensions/links.test.ts
@@ -6,6 +6,7 @@ import {
   validDurationValues,
 } from '../../Components/ServiceScene/Breakdowns/NumericFilterPopoverScene';
 import { addAdHocFilterUserInputPrefix } from '../variables';
+import { addCustomInputPrefixAndValueLabels, encodeFilter, getPath } from './utils';
 
 function getTestConfig(
   links: LinkConfigs,
@@ -776,36 +777,3 @@ describe('contextToLink', () => {
     });
   });
 });
-
-function encodeFilter(input: string) {
-  return encodeURIComponent(input)
-    .replace(/!/g, '%21')
-    .replace(/~/g, '%7E')
-    .replace(/%20/g, '+')
-    .replace(/\(/g, '%28')
-    .replace(/\)/g, '%29');
-}
-
-function getPath(options: {
-  slug: string;
-  expectedLabelFiltersUrlString?: string;
-  expectedMetadataString?: string;
-  expectedLineFiltersUrlString?: string;
-  expectedFieldsUrlString?: string;
-  expectedLevelsFilterUrlString?: string;
-}) {
-  return `/a/grafana-lokiexplore-app/explore/${options.slug}/logs?var-ds=123abc&from=1675828800000&to=1675854000000${
-    options.expectedLabelFiltersUrlString ?? ''
-  }${options.expectedMetadataString ?? ''}${options.expectedLineFiltersUrlString ?? ''}${
-    options.expectedFieldsUrlString ?? ''
-  }${options.expectedLevelsFilterUrlString ?? ''}`;
-}
-
-/**
- * Test helper method
- * Adds the custom input prefix
- * Adds the value to valueLabels
- */
-function addCustomInputPrefixAndValueLabels(value: string) {
-  return `${addAdHocFilterUserInputPrefix(value)},${value}`;
-}
