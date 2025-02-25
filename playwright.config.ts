bc4445c7937195c09e88c66f5224779ea552f8f5
Galen <galen.kistler@grafana.com>
2025-02-25 10:51:32 -0600

    chore: revert comment

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig<PluginOptions>({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Set parallel tests on CI. */
+  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
